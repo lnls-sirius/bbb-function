d5551a96a3a7ebfbb471416796d7bbd21548b9f3
Patricia Nallin <patricia.nallin@gmail.com>
2021-08-16 14:32:01 -0300

    logging bug

--- src/scripts/logger.py
+++ src/scripts/logger.py
@@ -3,13 +3,13 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def get_logger(name):
+def get_logger(name, logfile="/var/log/bbbfunction.log"):
     logger = logging.getLogger(name)
     formatter = logging.Formatter(
         "%(asctime)-15s - (%(name)s) %(levelname)s - %(message)s", datefmt="%d/%m/%Y %H:%M:%S"
     )
 
-    file_handler = RotatingFileHandler("/var/log/bbbfunction.log", maxBytes=15000000, backupCount=5)
+    file_handler = RotatingFileHandler(logfile, maxBytes=15000000, backupCount=5)
     file_handler.setFormatter(formatter)
 
     if not logger.hasHandlers():
